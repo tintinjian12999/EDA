c967c7b3d62a939013d9ed115a4d269625c34637
tintinjian <tintinjian@gmail.com>
2023-11-07 00:43:00 +0800

    first commit

---
+++ EDA_HW1/list_MRLC.h
@@ -0,0 +1,4 @@
+#include <iostream>
+#define NOP 0
+#define MULT 1
+#define ALU 2
---
+++ EDA_HW1/main.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+void read_line (std::vector<std::vector<int>>& data, std::ifstream& file, int& v, int& L); 
